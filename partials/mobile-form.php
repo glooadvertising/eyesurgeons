b5b52ab99dd8a37e48d898aef2d2b5fb1aaba3e0
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-26 15:42:20 +1100

    form

--- partials/mobile-form.php
+++ partials/mobile-form.php
@@ -1,13 +1,14 @@
 <div id="elpico" class="flex lg:hidden fixed bottom-0 w-full h-auto z-40">
 <div id="mobile-form-container" class="bg-blue-light flex flex-col p-8 lg:p-16 gap-12 w-full shadow-xl">
     <h2 class="text-white cursor-pointer text-xl lg:text-2xl font-bold flex justify-between">BOOK YOUR FREE ASSESMENT<span id="minus" class="hidden text-sm !font-light uppercase">Close</span></h2>
-    <form id="mobile-form" action="" class="hidden flex-col gap-6 transition-all">
+    <!-- <form id="mobile-form" action="" class="hidden flex-col gap-6 transition-all">
         <input type="text" class="p-4 h-14" placeholder="Full name">
         <input type="email" class="p-4 h-14" placeholder="Email address">
         <input type="phone" class="p-4 h-14" placeholder="Phone number">
         <textarea name="" id="" cols="30" rows="10" class="p-4" placeholder="Message"></textarea>
         <input type="button" value="Submit" class="btn inline-flex ml-auto cursor-pointer hover:ring-2 hover:ring-white hover:ring-inset">
-    </form>
+    </form> -->
+    <?php do_shortcode( '[wpforms id="1008"]' ); ?>
     <p id="mobile-form-paragraph" class="mb-4 text-white text-sm hidden transition-all">In your free assessment we carefully assess your condition, your lifestyle (including sports and hobbies), your eligibility, and overall eye health. We then recommend the treatment that best suits you individually.</p>
 </div>
 </div>
