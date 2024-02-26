cb571eb778c955ab9834c6741fda7525b745ef19
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-26 15:45:50 +1100

    js

--- scripts/new.js
+++ scripts/new.js
@@ -13,7 +13,7 @@ mobileContainer.addEventListener("click", () => {
 
 // mobile menu
 let trigger = document.querySelector('#mobile-menu-trigger')
-let mobileMenu = document.querySelector('#mobile-menu')
+let mobileMenu = document.querySelector('#wpforms-1008')
 let showing = document.querySelector('#show')
 let hiding = document.querySelector('#hide')
 let minus = document.querySelector('#minus')
