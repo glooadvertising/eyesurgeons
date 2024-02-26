a43e9423f2a820fa5b95bae1b1f0324cf94b5c73
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-27 09:10:14 +1100

    logo fixed

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <?php get_template_part( 'partials/mobile-menu' ); ?>
     <header class="bg-white p-8 flex justify-between items-center z-40">
         <a href="<?php echo site_url(); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/HES_logo.svg" alt="HES logo" class="w-36 lg:56">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/HES_logo.svg" alt="HES logo" class="w-36 lg:w-56">
         </a>
         <nav class="hidden xl:flex items-center gap-4">
             <?php wp_nav_menu(array(
