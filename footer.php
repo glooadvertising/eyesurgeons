7728f3b83aaa5ef144b771470dbdc93e01a10f18
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-28 16:49:48 +1100

    footer fixed

--- footer.php
+++ footer.php
@@ -1,29 +1,29 @@
 <footer class="bg-gray-lighter flex text-gray-dark min-h-1/2 tracking-wide">
-        <div class="grid w-full lg:flex mx-auto px-8 py-14 gap-8">
-            <div class="flex w-full lg:w-1/2 xl:w-1/2">
-                <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2">
+        <div class="flex flex-col lg:flex-row w-full mx-auto px-8 py-14 gap-8">
+            <div class="flex flex-col lg:flex-row gap-8 w-full md:w-full lg:w-1/2">
+                <div class="flex flex-col w-full xl:w-1/2">
                     <p class="mb-1"><strong>T: (03) 6210 6000</strong></p>
                     <p class="mb-1"><strong>HOBART</strong> - 182 Argyle Street</p>
                     <p class="mb-1"><strong>KINGSTON</strong> - Shop 93, Channel Court</p>
                     <p class="mb-1"><strong>ROSNY PARK</strong> - Level 1, 11 Bayfield Street</p>
                     <p class="mb-1"><strong>GLENORCHY</strong> - 3 / 370, Main Road</p>
                 </div>
-                <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2 justify-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/QIP_ACC.png" alt="" class="w-64 lg:mx-auto">
-                </div>
-            </div>            
-            <div class="grid gap-8 sm:flex lg:w-1/2 xl:w-1/2">
-                <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2">
+                <div class="flex flex-col w-full xl:w-1/2">
                     <?php wp_nav_menu([
                         'menu' => 'Footer Menu1',
                         'theme_location' => 'Footer menu'
                     ]); ?>
                 </div>
-                <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2">
+            </div>            
+            <div class="flex flex-col lg:flex-row gap-8 w-full md:w-full lg:w-1/2">
+                <div class="flex flex-col w-full xl:w-1/2">
                     <?php wp_nav_menu([
                         'menu' => 'Footer Menu2',
                         'theme_location' => 'Footer menu'
                     ]); ?>
+                </div>
+                <div class="flex flex-col w-full xl:w-1/2 justify-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/QIP_ACC.png" alt="" class="w-64 max-w-full">
                 </div>
             </div>
         </div>
