d971e30f8da3d789f698803e501202ce46f9abc2
Pablo Sazo <pablo@glooadvertising.com.au>
2024-02-28 15:59:54 +1100

    footer changes

--- footer.php
+++ footer.php
@@ -1,13 +1,17 @@
-<footer class="bg-gray-lighter flex text-gray-dark min-h-1/2 border-b-8 border-blue-dark tracking-wide">
+<footer class="bg-gray-lighter flex text-gray-dark min-h-1/2 tracking-wide">
         <div class="grid w-full lg:flex mx-auto px-8 py-14 gap-8">
-            <div class="flex flex-col w-full lg:w-1/3 xl:w-1/2">
-                <p class="mb-1"><strong>T: (03) 6210 6000</strong></p>
-                <p class="mb-1"><strong>HOBART</strong> - 182 Argyle Street</p>
-                <p class="mb-1"><strong>KINGSTON</strong> - Shop 93, Channel Court</p>
-                <p class="mb-1"><strong>ROSNY PARK</strong> - Level 1, 11 Bayfield Street</p>
-                <p class="mb-1"><strong>GLENORCHY</strong> - 3 / 370, Main Road</p>
-
-            </div>
+            <div class="flex w-full lg:w-1/3 xl:w-1/2">
+                <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2">
+                    <p class="mb-1"><strong>T: (03) 6210 6000</strong></p>
+                    <p class="mb-1"><strong>HOBART</strong> - 182 Argyle Street</p>
+                    <p class="mb-1"><strong>KINGSTON</strong> - Shop 93, Channel Court</p>
+                    <p class="mb-1"><strong>ROSNY PARK</strong> - Level 1, 11 Bayfield Street</p>
+                    <p class="mb-1"><strong>GLENORCHY</strong> - 3 / 370, Main Road</p>
+                </div>
+                <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2 justify-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/QIP_ACC.png" alt="" class="w-64 mx-auto">
+                </div>
+            </div>            
             <div class="grid gap-8 sm:flex lg:w-2/3 xl:w-1/2">
                 <div class="flex flex-col w-full sm:w-1/2 xl:w-1/2">
                     <?php wp_nav_menu([
@@ -24,6 +28,9 @@
             </div>
         </div>
     </footer>
+    <div class="bottom bg-blue-dark text-white text-sm p-1 flex content-center justify-center">
+        <div>&#169; Hobart Eye Surgeons</div>
+    </div>
 <?php wp_footer(); ?>
 </body>
 </html>
